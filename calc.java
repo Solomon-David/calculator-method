d73dc58ef4385381504600e0fb0c2ffbfe2865be
Solomon-David <74484746+Solomon-David@users.noreply.github.com>
2021-03-09 23:48:28 +0100

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,23 +1,52 @@
 class Calculator{
-public String calc(String a){
-String b="";
+public static String calc(String a){
 try{
 if(a.charAt(0)=='-'){
 a="0"+a;
 }
 
-
-String reg="[^0-9\\.]";
+else{
+a="0+"+a;
+}
+String b="";
+String reg="[\\(^A-Z^a-z^0-9\\.^√\\^]";
 Pattern p=Pattern.compile(reg);
 String []nu=p.split(a);
+      
+      for(int x=0; x<nu.length; x++){
+      String nuw="";
+if(nu[x].contains("√")){
+  nu[x]=nu[x].replace("√","");   
+    nu[x]=Math.sqrt(Double.parseDouble(nu[x]))+"";
+        }
+else if((nu[x].contains("^"))||(nu[x].contains("^")&&nu[x].contains("-"))){
+   
+ String[] half=nu[x].split("[\\^]");
+ 
+ nu[x]=Math.pow(Double.parseDouble(half[0]), Double.parseDouble(half[1]))+"";
+  
+   }
+        
+/*else if(nu[x].contains("Sin(")){
+          
+   //nu[x]=nu[x].substring(nu[x].indexOf("Sin("), nu[x].indexOf(")"));
+ nu[x]=nu[x].replace("Sin(", "");
+          
+          double d=Double.parseDouble(nu[x]);
+          d=Math.sin(d);
+          nu[x]=d+"";
+    Toast.makeText(context, "yes", Toast.LENGTH_SHORT).show();
+      }*/
+      nu[x]=nu[x];
+       
+  };
+      
 ArrayList<String> nums=new ArrayList<String>();
 Collections.addAll(nums,nu);
-
 int l=nums.size();
-ArrayList<Float> num=new ArrayList<Float>();
-
+float[] num=new float[l];
 for(int d=0;d<l;d++){
-num.add(Float.parseFloat(nums.get(d)));
+num[d]=Float.parseFloat(nums.get(d));
 }
  String c="";
 Matcher m=p.matcher(a);
@@ -25,42 +54,38 @@ for(int x=0;x<l;x++){
 if(m.find()){
 c+=m.group();
 }
-else{
-b+=nums.get(x);
+
 }
-}
-float sum=num.get(0);
+float sum=num[0];
 for(int e=1;e<l;e++){
 
 if(Character.toString(c.charAt(e-1)).equals("+")){
-sum+=num.get(e);
+sum+=num[e];
 }
 
 else if(Character.toString(c.charAt(e-1)).equals("-")){
-sum-=num.get(e);
+sum-=num[e];
 }
 
 else if(Character.toString(c.charAt(e-1)).equals("x")){
-sum*=num.get(e);
+sum*=num[e];
 }
-/*else if(Character.toString(c.charAt(e-1)).equals("^")){
-
-num.get(e)=Math.pow(num.get(e-1))
-}*/
 
 else if(Character.toString(c.charAt(e-1)).equals("÷")){
-sum/=num.get(e);
+sum/=num[e];
 }
+else{
+sum=num[0];
+        }
 
-b=Float.toString(sum);
-}
 
-
-}
-catch(Exception e){
-Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
-}
+b=new DecimalFormat("@@@@").format(sum)+"";
+ b=Float.parseFloat(b)+"";
+      }
 return b;
 }
-
+catch(Exception e){
+return e.toString();
+}
+}
 }
